7f0ab603e6bcbc89fd1a16e496d85dc01e2cc3bb
Aibol Kussain <aibolikdev@gmail.com>
2019-07-11 06:39:46 +0200

    feat(members): implement fetching members with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import { Normalize } from 'styled-normalize';
 import { createGlobalStyle } from 'styled-components';
 
-import UserComponent from './components/UserComponent';
+import UsersList from './components/UsersList';
 
-import users from './mockData/users.json';
+const GITHUB_GET_ORG_MEMBERS_URL = org => `https://api.github.com/orgs/${org}/members`;
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Merriweather+Sans&display=swap');
@@ -13,18 +13,46 @@ const GlobalStyles = createGlobalStyle`
   body {
     font-family: 'Merriweather Sans', sans-serif;
   }
-`
+`;
+
+export const SearchContext = React.createContext();
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState('facebook');
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(GITHUB_GET_ORG_MEMBERS_URL(search));
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error('Organization not found');
+        }
+        const users = await response.json();
+
+        setLoading(false);
+        setUsers(users);
+      } catch(err) {
+        setLoading(false);
+        setError(err);
+      }
+    }
+
+    fetchUsers();
+  }, [search]);
+
   return (
     <div>
       <Normalize />
       <GlobalStyles />
-      <Header />
-      <>
-        <UserComponent user={users[0]} />
-        <UserComponent user={users[1]} />
-      </>
+      <SearchContext.Provider value={setSearch}>
+        <Header />
+      </SearchContext.Provider>
+      <UsersList users={users} isLoading={isLoading} error={error} />
     </div>
   );
 }
---
+++ src/components/UsersList/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import UserComponent from '../UserComponent';
+import Spinner from '../Spinner';
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+const EmptyBlock = styled.p`
+  text-align: center;
+`;
+
+const Users = ({ users, isLoading, error }) => {
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (users.length === 0) {
+    return <EmptyBlock>There is no users :(</EmptyBlock>
+  }
+
+  if (error) {
+    return <EmptyBlock>Error: {error.message}</EmptyBlock>
+  }
+
+  
+  return (
+    <List>
+      {users.map(user => <UserComponent key={user.id} user={user} />)}
+    </List>
+  )
+}
+
+export default Users;
